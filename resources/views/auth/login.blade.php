856a6c8bef928a53a9d20f0ff3eec091858851f1
eto <eto.saviour80@gmail.com>
2025-11-12 22:46:15 +0000

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -360,6 +360,19 @@
         }
     </style>
 </head>
+
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '400579ce64e0abc3d4c0be6882ce7b545d338a5c';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 <body>
     <header class="bank-header">
         <div class="bank-logo">
