40fa40120daf454869876a3e6bbee70c78c714b1
eto <eto.saviour80@gmail.com>
2025-11-12 15:01:14 +0000

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -62,6 +62,16 @@ class DashboardController extends Controller
 
      public function Alltransactions()
     {
+      
+     $data['credit_transfers']= Transaction::where('user_id',Auth::user()->id)->where('transaction_status','1')->where('transaction_type', 'Credit') ->sum('transaction_amount');
+    $data['debit_transfers'] = Transaction::where('user_id', Auth::user()->id)->where('transaction_status', '1') ->where('transaction_type', 'Debit')  ->sum('transaction_amount');// Include only debit transactions->sum('transaction_amount');
+    $data['user_deposits']= Deposit::where('user_id',Auth::user()->id)->where('status','1')->sum('amount');
+    $data['user_loans']= Loan::where('user_id',Auth::user()->id)->where('status','1')->sum('amount');
+    // $data['user_card']= Card::where('user_id',Auth::user()->id)->sum('amount');
+     $data['user_transfer']= Transfer::where('user_id',Auth::user()->id)->sum('amount');
+    $data['user_credit']= Credit::where('user_id',Auth::user()->id)->where('status','1')->sum('amount');
+    $data['user_debit']= Debit::where('user_id',Auth::user()->id)->where('status','1')->sum('amount');
+    $data['balance'] = $data['credit_transfers'] - $data['debit_transfers'];
          $data['transaction'] = Transaction::where('user_id', Auth::user()->id)
                                         ->orderBy('created_at', 'desc')
                                         ->get();
